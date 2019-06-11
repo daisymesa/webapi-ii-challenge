d789fc1b0e95343e3a215acdeea24a18beeb6e3e
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-06-11 17:42:37 -0400

    Set up server port and dataRouter.js file

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ server.use('/api/posts', dataRouter);
 server.get('/', (req, res) => {
     res.send(`
         <h2>Building RESTful APIs with Express</h2>
-        `)
-})
+        `);
+});
 
 module.exports = server;
